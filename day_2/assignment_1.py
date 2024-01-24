cbd4f40e200d7d845b1a81f4c711365588c43513
Prathamesh Chatte <chatteprathamesh01@gmail.com>
2024-01-24 17:37:34 +0530

    added  first assignment in day2

---
+++ day_2/assignment_1.py
@@ -0,0 +1,45 @@
+"""
+Input a list of Numbers from user i.e take integer inputs and append to list.
+
+Find the smallest and Second Smallest Numbers from that list
+"""
+"""---------------------------------------------------------------"""
+
+#if we don't want to use default sort function then use, we can go with sorting algorithm 
+# for i in range(len(num_list)):
+#     for j in range(len(num_list)):
+#         if num_list[i] < num_list[j]:
+#             temp = num_list[i]
+#             num_list[i] = num_list[j]
+#             num_list[j] = temp
+
+
+"""----------------------using default function: sort()-----------------------------------------"""
+num_list = [] #created a numbers list
+
+# use of while loop for taking multiple input values from user
+while True:
+    usr_input = input("Enter a number or (q to quit): ")
+    if usr_input.lower() == "q":
+        break
+    try:
+        num = int(usr_input)
+        num_list.append(num)#append input in the list
+    except ValueError :
+        print("Invalid input. Please enter an integer number.")
+
+if not num_list:
+    print("No numbers were entered.")
+else :
+    print("Input Number List : ", num_list)
+
+    #to remove the duplicates, lsit is converted to set and again to list
+    num_list = list(set(num_list))
+    num_list.sort() #sort function for sorting numbers in assending order
+
+smallest = num_list[0]
+second_smallest = num_list[1] if len(num_list) > 1 else None
+
+print("Smallest number = ", smallest)
+print("Second smallest number = ", second_smallest)
+    
